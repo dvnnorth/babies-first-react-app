dfd1e0b15c3061e477a81c850b5336cb22720e0a
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-09-10 21:23:27 -0500

    Rendered a page as a component. Used bootstrap

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const HelloDiv = () => (
-    <div>
+    <Fragment>
         <h1>Hello! My name is Devin!</h1>
         <ul>
             <li>Food</li>
             <li>Other Stuff</li>
             <li>*~*CoMpUtEr StUfF*~*</li>
         </ul>
-    </div>
+    </Fragment>
 );
 
 export default HelloDiv;
